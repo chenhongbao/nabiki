a974250b04a601501479379f0bbb700d921f6773
陈宏葆 <chenhongbao@outlook.com>
2020-08-17 07:37:48 +0800

    Clear dir before writing new settlement data.

--- centre/main/com/nabiki/centre/user/core/UserManager.java
+++ centre/main/com/nabiki/centre/user/core/UserManager.java
@@ -154,9 +154,21 @@ public class UserManager implements Renewable {
         position.SettlementPrice = 0;
     }
 
+    // Clear all files and dirs under the specified path.
+    private void clearDir(Path dir) throws IOException {
+        if (dir == null || !dir.toFile().isDirectory())
+            return;
+        for (var f : Objects.requireNonNull(dir.toFile().listFiles()))
+            if (f.isFile())
+                Files.deleteIfExists(f.toPath());
+            else if (f.isDirectory())
+                clearDir(f.toPath());
+    }
+
     private void write(Path dir) throws IOException {
         for (var user : this.users.values()) {
             var userDir = Path.of(dir.toString(), user.getUserID());
+            clearDir(userDir);
             writeUser(userDir, user);
         }
     }
