c1ea4377a1aa16f25debe54b346649755f99f88c
陈宏葆 <chenhongbao@outlook.com>
2020-10-12 08:21:09 +0800

    Better logging msg.

--- nabiki-centre/src/main/java/com/nabiki/centre/ctp/TickProvider.java
+++ nabiki-centre/src/main/java/com/nabiki/centre/ctp/TickProvider.java
@@ -1,22 +1,22 @@
 /*
- * Copyright (c) 2020 Hongbao Chen <chenhongbao@outlook.com>
- *
+ * Copyright (c) 2020-2020. Hongbao Chen <chenhongbao@outlook.com>
+ *  *
  * Licensed under the  GNU Affero General Public License v3.0 and you may not use
  * this file except in compliance with the  License. You may obtain a copy of the
  * License at
- *
+ *  *
  *                    https://www.gnu.org/licenses/agpl-3.0.txt
- *
+ *  *
  * Permission is hereby  granted, free of charge, to any  person obtaining a copy
  * of this software and associated  documentation files (the "Software"), to deal
  * in the Software  without restriction, including without  limitation the rights
  * to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
  * copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
  * furnished to do so, subject to the following conditions:
- *
+ *  *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ *  *
  * THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
  * IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
  * FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
@@ -381,7 +381,8 @@ public class TickProvider {
           count = 0;
         }
       }
-      global.getLogger().warning("instruments aren't subscribed" + msgBlock);
+      global.getLogger().warning(
+          r.size() + " instruments have no sub rsp: " + msgBlock);
     }
     // Clear.
     this.subscribed.clear();
